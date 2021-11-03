61864bb19330d515b3da1c918a0b897b74ac28ab
remi-ui <r.m.thuss@hotmail.com>
2021-11-03 11:45:55 +0100

    added automatic ouput txt file and command line argument for evaluate file

--- evaluate.py
+++ evaluate.py
@@ -10,12 +10,20 @@ import train
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', nargs='?', default='svm', choices=['naive_bayes', 'svm', 'svm_optimized', 'lstm', 'bert'])
+    parser.add_argument('--eval', default = 'newspapers_157_upsampled_test.json', type=str)
     args = parser.parse_args()
     return args
 
 
 def model_report(model):
     Y_dev, predictions = predict.predict_model(model)
+    i=0
+    with open('experiments/prediction_vs_gold.txt', 'w') as f:
+        for item in Y_dev:
+            f.write("{} - {}\n".format(item, Y_dev[i]))
+            i+=1
+    f.close()
+
     class_report = report(Y_dev, predictions, digits=3)
     print(class_report)
     return f1_score(Y_dev, predictions, average='weighted')
